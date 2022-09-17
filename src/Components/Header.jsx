41c441ced85c8cae663fc4a0812d7eff1bf491a1
Justinas <justasrimavicius@gmail.com>
2022-09-17 16:07:34 +0300

    fixed links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,7 @@ import { Button } from '@mui/material';
 import { useEffect } from 'react';
 
 function Header(props) {
-    useEffect(()=>{
 
-        console.log(props.homeView.homeView)
-    },[props.homeView.homeView])
     return (
         <header>
             <span>Curly Braces</span>
@@ -17,7 +14,7 @@ function Header(props) {
                 <Button className='header-nav-button' style={{fontWeight:'bold'}} onClick={()=>{props.homeView.setHomeContent('test')}}>Test your knowledge</Button>
             </div>
             <div className='header-user-info'>
-                <Button onClick={()=>{window.location.href='http://localhost:3000'}}>Log out</Button>
+                <Button onClick={()=>{window.location.href='https://curly-braces.netlify.app/'}}>Log out</Button>
             </div>
         </header>
     );

df4ebc13cfc9e2b38568d7c83daf6958dbf1137c
Justinas <justasrimavicius@gmail.com>
2022-10-06 19:12:42 +0200

    Added favicon, mini fixes.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
 
     return (
         <footer>
+            <div className='divider'></div>
             @2022 <a href='https://github.com/Justasrimavicius/curly-braces'>github.com/Justasrimavicius/curly-braces</a>
         </footer>
     );
